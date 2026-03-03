05696e55831fcaa00f751170490bc209c58c2c1d
snitk <snitka.zhenya@gmail.com>
2026-03-04 00:01:39 +0200

    Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -8,11 +8,18 @@ number = input("6")
 average = (2 + 4 + 6) / 3
 print(average)
 
-
 total_minutes = input("135")
 number1 = 135 // 60
 number2 = 135 % 60
 print(number1,number2)
+
+price = input("1000")
+discont = input("15")
+procent = 15 / 100
+total_price = 1000 - (1000 * 15 / 100)
+print(total_price)
+
+
 
 
 
